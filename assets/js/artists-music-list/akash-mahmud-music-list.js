fe6479c84d5c8f9570e13d8954f03d396da569cb
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-02-07 23:10:16 +0600

    Update akash-mahmud-music-list.js

--- assets/js/artists-music-list/akash-mahmud-music-list.js
+++ assets/js/artists-music-list/akash-mahmud-music-list.js
@@ -1,65 +1,66 @@
 let allMusic = [
   {
-    name: "১. মাহে রমজান এলো বছর ঘুর",
+    name: "১. ৬৪ জেলা নিয়ে গান",
     artist: "আকাশ মাহমুদ",
     img: "https://masumbillah6778.github.io/images/thumbnail/akash-mahmud.jpg",
     src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/64-jelar-gaan.mp3",
   },
   {
-    name: "২. মাহে",
+    name: "২. আমি টাকার এক মেসিন",
     artist: "আকাশ মাহমুদ",
     img: "https://masumbillah6778.github.io/images/thumbnail/akash-mahmud.jpg",
     src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/takar-ak-mesin.mp3",
   },
   {
-    name: "৩. মাহে ",
+    name: "৩. আমার কাজলা পাখি রে",
     artist: "আকাশ মাহমুদ",
     img: "https://masumbillah6778.github.io/images/thumbnail/akash-mahmud.jpg",
     src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/amar-kajla-pakhi-re.mp3",
   },
   {
-    name: "৪. মাহ",
+    name: "৪. একখান বান্ধা প্রেমের ঘর",
     artist: "আকাশ মাহমুদ",
     img: "https://masumbillah6778.github.io/images/thumbnail/akash-mahmud.jpg",
     src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/akkhan-bandha-premer-ghor.mp3",
   },
   {
-    name: "৫. মাহে রমজান",
+    name: "৫. বড় লোকের মাইয়া",
     artist: "আকাশ মাহমুদ",
     img: "https://masumbillah6778.github.io/images/thumbnail/akash-mahmud.jpg",
     src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/borolok-er-maiya.mp3",
   },
   {
-    name: "৬. মাহে",
+    name: "৬. তোর পরানে পরান বান্ধা",
     artist: "আকাশ মাহমুদ",
     img: "https://masumbillah6778.github.io/images/thumbnail/akash-mahmud.jpg",
     src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/dil-dorodia.mp3",
   },
   {
-    name: "৭. মাহে ",
+    name: "৭. এক মন তুমি দিবা কয়জনারে ",
     artist: "আকাশ মাহমুদ",
     img: "https://masumbillah6778.github.io/images/thumbnail/akash-mahmud.jpg",
     src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/ek-mon-tomi-koijonere.mp3",
   },
   {
-    name: "৮. মাহ",
+    name: "৮. একলার সংসার",
     artist: "আকাশ মাহমুদ",
     img: "https://masumbillah6778.github.io/images/thumbnail/akash-mahmud.jpg",
     src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/eklar-sonshar.mp3",
   },
   {
-    name: "৯. মাহে রমজান",
+    name: "৯. তোর সাথে মোর গোপন পিরিত",
     artist: "আকাশ মাহমুদ",
     img: "https://masumbillah6778.github.io/images/thumbnail/akash-mahmud.jpg",
     src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/gopon-pirit.mp3",
   },
   {
-    name: "১০. মাহে",
+    name: "১০. জাদু করিলা",
     artist: "আকাশ মাহমুদ",
     img: "https://masumbillah6778.github.io/images/thumbnail/akash-mahmud.jpg",
     src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/jadu-korila.mp3",
   },
 ];
+
 
 
 
