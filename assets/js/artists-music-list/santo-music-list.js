5f51009541a9735ecd10169c939a0eb757829e3b
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-02-08 17:32:45 +0600

    Update santo-music-list.js

--- assets/js/artists-music-list/santo-music-list.js
+++ assets/js/artists-music-list/santo-music-list.js
@@ -5,4 +5,40 @@ let allMusic = [
     img: "https://masumbillah6778.github.io/images/thumbnail/santo.jpg",
     src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/64-jelar-gaan.mp3",
   },
+  {
+    name: "১. ৬৪ জেলা নিয়ে গান",
+    artist: "শান্ত",
+    img: "https://masumbillah6778.github.io/images/thumbnail/santo.jpg",
+    src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/64-jelar-gaan.mp3",
+  },
+  {
+    name: "১. ৬৪ জেলা নিয়ে গান",
+    artist: "শান্ত",
+    img: "https://masumbillah6778.github.io/images/thumbnail/santo.jpg",
+    src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/64-jelar-gaan.mp3",
+  },
+  {
+    name: "১. ৬৪ জেলা নিয়ে গান",
+    artist: "শান্ত",
+    img: "https://masumbillah6778.github.io/images/thumbnail/santo.jpg",
+    src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/64-jelar-gaan.mp3",
+  },
+  {
+    name: "১. ৬৪ জেলা নিয়ে গান",
+    artist: "শান্ত",
+    img: "https://masumbillah6778.github.io/images/thumbnail/santo.jpg",
+    src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/64-jelar-gaan.mp3",
+  },
+  {
+    name: "১. ৬৪ জেলা নিয়ে গান",
+    artist: "শান্ত",
+    img: "https://masumbillah6778.github.io/images/thumbnail/santo.jpg",
+    src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/64-jelar-gaan.mp3",
+  },
+  {
+    name: "১. ৬৪ জেলা নিয়ে গান",
+    artist: "শান্ত",
+    img: "https://masumbillah6778.github.io/images/thumbnail/santo.jpg",
+    src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/64-jelar-gaan.mp3",
+  },
   ];
