6dfeca506224264441d14b586f9cffb65bd2888b
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-02-08 17:54:09 +0600

    Update santo-music-list.js

--- assets/js/artists-music-list/santo-music-list.js
+++ assets/js/artists-music-list/santo-music-list.js
@@ -3,6 +3,42 @@ let allMusic = [
     name: "১. ৬৪ জেলা নিয়ে গান",
     artist: "শান্ত",
     img: "https://masumbillah6778.github.io/images/thumbnail/santo.jpg",
-    src: "https://github.com/masumbillah6778bd/santo/raw/main/64-jelar-gaan.mp3",
+    src: "https://masumbillah6778bd.github.io/santo/amar-barir-asmni-dia.mp3",
+  },
+  {
+    name: "১. ৬৪ জেলা নিয়ে গান",
+    artist: "শান্ত",
+    img: "https://masumbillah6778.github.io/images/thumbnail/santo.jpg",
+    src: "https://masumbillah6778bd.github.io/santo/ami-sorbonasi-priya.mp3",
+  },
+  {
+    name: "১. ৬৪ জেলা নিয়ে গান",
+    artist: "শান্ত",
+    img: "https://masumbillah6778.github.io/images/thumbnail/santo.jpg",
+    src: "https://masumbillah6778bd.github.io/santo/madhuri-ke-chai.mp3",
+  },
+  {
+    name: "১. ৬৪ জেলা নিয়ে গান",
+    artist: "শান্ত",
+    img: "https://masumbillah6778.github.io/images/thumbnail/santo.jpg",
+    src: "https://masumbillah6778bd.github.io/santo/madhuri-morar-age.mp3",
+  },
+  {
+    name: "১. ৬৪ জেলা নিয়ে গান",
+    artist: "শান্ত",
+    img: "https://masumbillah6778.github.io/images/thumbnail/santo.jpg",
+    src: "https://masumbillah6778bd.github.io/santo/para-sorshi-shobai-bole.mp3",
+  },
+  {
+    name: "১. ৬৪ জেলা নিয়ে গান",
+    artist: "শান্ত",
+    img: "https://masumbillah6778.github.io/images/thumbnail/santo.jpg",
+    src: "https://masumbillah6778bd.github.io/santo/tomra-madhrir-naam.mp3",
+  },
+  {
+    name: "১. ৬৪ জেলা নিয়ে গান",
+    artist: "শান্ত",
+    img: "https://masumbillah6778.github.io/images/thumbnail/santo.jpg",
+    src: "https://masumbillah6778bd.github.io/santo/amar-barir-asmni-dia.mp3",
   },
 ];
