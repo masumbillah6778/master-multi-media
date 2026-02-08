26d21ea1027d2974488127ffcbf852bbfb02d122
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-02-08 17:17:01 +0600

    Update santo-music-list.js.js

--- assets/js/artists-music-list/santo-music-list.js.js
+++ assets/js/artists-music-list/santo-music-list.js.js
@@ -1,1 +1,8 @@
-
+let allMusic = [
+  {
+    name: "১. ৬৪ জেলা নিয়ে গান",
+    artist: "আকাশ মাহমুদ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/santo.jpg",
+    src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/64-jelar-gaan.mp3",
+  },
+  ];
