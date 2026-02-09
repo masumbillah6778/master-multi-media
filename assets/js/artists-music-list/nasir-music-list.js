8afea876e139e813fde088ecd0f4d7cbe3a5f1bc
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-02-09 12:01:00 +0600

    Update nasir-music-list.js

--- assets/js/artists-music-list/nasir-music-list.js
+++ assets/js/artists-music-list/nasir-music-list.js
@@ -1,8 +1,14 @@
 let allMusic = [
   {
     name: "১. ৬৪ জেলা নিয়ে গান",
-    artist: "নাসির",
+    artist: "নাসির / নদী",
     img: "https://masumbillah6778.github.io/images/thumbnail/nasir.jpg",
     src: "https://github.com/masumbillah6778bd/nasir/raw/main/kodal-daia-mati-khura.mp3",
   },
-  ];
+  {
+    name: "১. ৬৪ জেলা নিয়ে গান",
+    artist: "নাসির  [নদী]",
+    img: "https://masumbillah6778.github.io/images/thumbnail/nasir.jpg",
+    src: "https://github.com/masumbillah6778bd/nasir/raw/main/kodal-daia-mati-khura.mp3",
+  },
+];
