ef37f36f69c9607e58b5c38e516589acfda5c358
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-02-09 11:52:30 +0600

    Update nasir-music-list.js.js

--- assets/js/artists-music-list/nasir-music-list.js.js
+++ assets/js/artists-music-list/nasir-music-list.js.js
@@ -1,7 +1,7 @@
 let allMusic = [
   {
     name: "১. ৬৪ জেলা নিয়ে গান",
-    artist: "আকাশ মাহমুদ",
+    artist: "নাসির",
     img: "https://masumbillah6778.github.io/images/thumbnail/nasir.jpg",
     src: "https://github.com/masumbillah6778bd/nasir/raw/main/kodal-daia-mati-khura.mp3",
   },
