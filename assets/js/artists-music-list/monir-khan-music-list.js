e4ba435e88daf19a5ee01e360d7ac8806d47f3e9
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-02-10 23:06:22 +0600

    Delete assets/js/artists-music-list/monir-khan-music-list.js

--- assets/js/artists-music-list/monir-khan-music-list.js
+++
@@ -1,9 +0,0 @@
-let allMusic = [
-  {
-    name: "১. শেষ ঠিকানা",
-    artist: "মনির খান / আবার কেনো পিছু ডাকো অ্যালবাম",
-    img: "https://masumbillah6778.github.io/images/thumbnail/monir-khan.jpg",
-    src: "https://masumbillah6778bd.github.io/monir-khan/abar-kano-pisu-dako/shesh-thikanay.mp3",
-  },
-],
-
