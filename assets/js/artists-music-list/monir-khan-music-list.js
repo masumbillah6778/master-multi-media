e98bdb668aa1acedbee426ae6103028e69fc0bbe
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-02-10 22:41:22 +0600

    Update monir-khan-music-list.js

--- assets/js/artists-music-list/monir-khan-music-list.js
+++ assets/js/artists-music-list/monir-khan-music-list.js
@@ -1,1 +1,9 @@
-https://masumbillah6778bd.github.io/monir-khan/abar-kano-pisu-dako/shesh-thikanay.mp3
+let allMusic = [
+  {
+    name: "১. ৬৪ জেলা নিয়ে গান",
+    artist: "নাসির / নদী অ্যালবাম",
+    img: "https://masumbillah6778.github.io/images/thumbnail/monir-khan.jpg",
+    src: "https://masumbillah6778bd.github.io/monir-khan/abar-kano-pisu-dako/shesh-thikanay.mp3",
+  },
+],
+
