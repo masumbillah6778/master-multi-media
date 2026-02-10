7baa5edde2f3f4a57d0fab7b1116d43ab551cb13
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-02-11 03:50:04 +0600

    Update asif-music-list.js

--- assets/js/music-list/asif-music-list.js
+++ assets/js/music-list/asif-music-list.js
@@ -5,4 +5,58 @@ let allMusic = [
     img: "https://masumbillah6778.github.io/images/thumbnail/asif.jpg",
     src: "https://masumbillah6778bd.github.io/asif/o-priya-tumi-kothay/o-priya-tumi-kothay.mp3",
   },
+  {
+    name: "২. ও প্রিয়া তুমি কোথায়",
+    artist: "আসিফ / ও প্রিয়া তুমি কোথায়",
+    img: "https://masumbillah6778.github.io/images/thumbnail/asif.jpg",
+    src: "https://masumbillah6778bd.github.io/asif/o-priya-tumi-kothay/o-priya-tumi-kothay.mp3",
+  },
+  {
+    name: "৩. ও প্রিয়া তুমি কোথায়",
+    artist: "আসিফ / ও প্রিয়া তুমি কোথায়",
+    img: "https://masumbillah6778.github.io/images/thumbnail/asif.jpg",
+    src: "https://masumbillah6778bd.github.io/asif/o-priya-tumi-kothay/o-priya-tumi-kothay.mp3",
+  },
+  {
+    name: "৪. ও প্রিয়া তুমি কোথায়",
+    artist: "আসিফ / ও প্রিয়া তুমি কোথায়",
+    img: "https://masumbillah6778.github.io/images/thumbnail/asif.jpg",
+    src: "https://masumbillah6778bd.github.io/asif/o-priya-tumi-kothay/o-priya-tumi-kothay.mp3",
+  },
+  {
+    name: "৫. ও প্রিয়া তুমি কোথায়",
+    artist: "আসিফ / ও প্রিয়া তুমি কোথায়",
+    img: "https://masumbillah6778.github.io/images/thumbnail/asif.jpg",
+    src: "https://masumbillah6778bd.github.io/asif/o-priya-tumi-kothay/o-priya-tumi-kothay.mp3",
+  },
+  {
+    name: "৬. ও প্রিয়া তুমি কোথায়",
+    artist: "আসিফ / ও প্রিয়া তুমি কোথায়",
+    img: "https://masumbillah6778.github.io/images/thumbnail/asif.jpg",
+    src: "https://masumbillah6778bd.github.io/asif/o-priya-tumi-kothay/o-priya-tumi-kothay.mp3",
+  },
+  {
+    name: "৭. ও প্রিয়া তুমি কোথায়",
+    artist: "আসিফ / ও প্রিয়া তুমি কোথায়",
+    img: "https://masumbillah6778.github.io/images/thumbnail/asif.jpg",
+    src: "https://masumbillah6778bd.github.io/asif/o-priya-tumi-kothay/o-priya-tumi-kothay.mp3",
+  },
+  {
+    name: "৮. ও প্রিয়া তুমি কোথায়",
+    artist: "আসিফ / ও প্রিয়া তুমি কোথায়",
+    img: "https://masumbillah6778.github.io/images/thumbnail/asif.jpg",
+    src: "https://masumbillah6778bd.github.io/asif/o-priya-tumi-kothay/o-priya-tumi-kothay.mp3",
+  },
+  {
+    name: "৯. ও প্রিয়া তুমি কোথায়",
+    artist: "আসিফ / ও প্রিয়া তুমি কোথায়",
+    img: "https://masumbillah6778.github.io/images/thumbnail/asif.jpg",
+    src: "https://masumbillah6778bd.github.io/asif/o-priya-tumi-kothay/o-priya-tumi-kothay.mp3",
+  },
+  {
+    name: "১০. ও প্রিয়া তুমি কোথায়",
+    artist: "আসিফ / ও প্রিয়া তুমি কোথায়",
+    img: "https://masumbillah6778.github.io/images/thumbnail/asif.jpg",
+    src: "https://masumbillah6778bd.github.io/asif/o-priya-tumi-kothay/o-priya-tumi-kothay.mp3",
+  },
 ];
