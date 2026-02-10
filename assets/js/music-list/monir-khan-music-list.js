354a9c83cd68848beb309be862a57e6a52cde2e6
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-02-10 23:49:39 +0600

    Update monir-khan-music-list.js

--- assets/js/music-list/monir-khan-music-list.js
+++ assets/js/music-list/monir-khan-music-list.js
@@ -5,5 +5,59 @@ let allMusic = [
     img: "https://masumbillah6778.github.io/images/thumbnail/monir-khan.jpg",
     src: "https://masumbillah6778bd.github.io/monir-khan/abar-kano-pisu-dako/shesh-thikanay.mp3",
   },
+  {
+    name: "২. শেষ ঠিকানা",
+    artist: "মনির খান / আবার কেনো পিছু ডাকো অ্যালবাম",
+    img: "https://masumbillah6778.github.io/images/thumbnail/monir-khan.jpg",
+    src: "https://masumbillah6778bd.github.io/monir-khan/abar-kano-pisu-dako/shesh-thikanay.mp3",
+  },
+  {
+    name: "৩. শেষ ঠিকানা",
+    artist: "মনির খান / আবার কেনো পিছু ডাকো অ্যালবাম",
+    img: "https://masumbillah6778.github.io/images/thumbnail/monir-khan.jpg",
+    src: "https://masumbillah6778bd.github.io/monir-khan/abar-kano-pisu-dako/shesh-thikanay.mp3",
+  },
+  {
+    name: "৪. শেষ ঠিকানা",
+    artist: "মনির খান / আবার কেনো পিছু ডাকো অ্যালবাম",
+    img: "https://masumbillah6778.github.io/images/thumbnail/monir-khan.jpg",
+    src: "https://masumbillah6778bd.github.io/monir-khan/abar-kano-pisu-dako/shesh-thikanay.mp3",
+  },
+  {
+    name: "৫. শেষ ঠিকানা",
+    artist: "মনির খান / আবার কেনো পিছু ডাকো অ্যালবাম",
+    img: "https://masumbillah6778.github.io/images/thumbnail/monir-khan.jpg",
+    src: "https://masumbillah6778bd.github.io/monir-khan/abar-kano-pisu-dako/shesh-thikanay.mp3",
+  },
+  {
+    name: "৬. শেষ ঠিকানা",
+    artist: "মনির খান / আবার কেনো পিছু ডাকো অ্যালবাম",
+    img: "https://masumbillah6778.github.io/images/thumbnail/monir-khan.jpg",
+    src: "https://masumbillah6778bd.github.io/monir-khan/abar-kano-pisu-dako/shesh-thikanay.mp3",
+  },
+  {
+    name: "৭. শেষ ঠিকানা",
+    artist: "মনির খান / আবার কেনো পিছু ডাকো অ্যালবাম",
+    img: "https://masumbillah6778.github.io/images/thumbnail/monir-khan.jpg",
+    src: "https://masumbillah6778bd.github.io/monir-khan/abar-kano-pisu-dako/shesh-thikanay.mp3",
+  },
+  {
+    name: "৮. শেষ ঠিকানা",
+    artist: "মনির খান / আবার কেনো পিছু ডাকো অ্যালবাম",
+    img: "https://masumbillah6778.github.io/images/thumbnail/monir-khan.jpg",
+    src: "https://masumbillah6778bd.github.io/monir-khan/abar-kano-pisu-dako/shesh-thikanay.mp3",
+  },
+  {
+    name: "৯. শেষ ঠিকানা",
+    artist: "মনির খান / আবার কেনো পিছু ডাকো অ্যালবাম",
+    img: "https://masumbillah6778.github.io/images/thumbnail/monir-khan.jpg",
+    src: "https://masumbillah6778bd.github.io/monir-khan/abar-kano-pisu-dako/shesh-thikanay.mp3",
+  },
+  {
+    name: "১০. শেষ ঠিকানা",
+    artist: "মনির খান / আবার কেনো পিছু ডাকো অ্যালবাম",
+    img: "https://masumbillah6778.github.io/images/thumbnail/monir-khan.jpg",
+    src: "https://masumbillah6778bd.github.io/monir-khan/abar-kano-pisu-dako/shesh-thikanay.mp3",
+  },
 ];
 
