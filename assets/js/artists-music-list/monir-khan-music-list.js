e10bf0999c8e76ec8af0a272a088bf0d5fb51436
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-02-10 22:53:34 +0600

    Update monir-khan-music-list.js

--- assets/js/artists-music-list/monir-khan-music-list.js
+++ assets/js/artists-music-list/monir-khan-music-list.js
@@ -1,7 +1,7 @@
 let allMusic = [
   {
-    name: "১. ৬৪ জেলা নিয়ে গান",
-    artist: "নাসির / নদী অ্যালবাম",
+    name: "১. শেষ ঠিকানা",
+    artist: "মনির খান / আবার কেনো পিছু ডাকো অ্যালবাম",
     img: "https://masumbillah6778.github.io/images/thumbnail/monir-khan.jpg",
     src: "https://masumbillah6778bd.github.io/monir-khan/abar-kano-pisu-dako/shesh-thikanay.mp3",
   },
