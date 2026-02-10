681c798d0b611a615ac6e28c2d242ac379019574
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-02-11 03:41:24 +0600

    Update asif-music-list.js

--- assets/js/music-list/asif-music-list.js
+++ assets/js/music-list/asif-music-list.js
@@ -1,1 +1,8 @@
-
+let allMusic = [
+  {
+    name: "১. ও প্রিয়া তুমি কোথায়",
+    artist: "মনির খান / আবার কেনো পিছু ডাকো অ্যালবাম",
+    img: "https://masumbillah6778.github.io/images/thumbnail/asif.jpg",
+    src: "https://masumbillah6778bd.github.io/monir-khan/abar-kano-pisu-dako/okanay.mp3",
+  },
+];
