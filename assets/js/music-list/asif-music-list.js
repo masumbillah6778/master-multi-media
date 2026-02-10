d7e6cbaac94648e86ecab547ca77863c3d48acc8
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-02-11 03:48:22 +0600

    Update asif-music-list.js

--- assets/js/music-list/asif-music-list.js
+++ assets/js/music-list/asif-music-list.js
@@ -3,6 +3,6 @@ let allMusic = [
     name: "১. ও প্রিয়া তুমি কোথায়",
     artist: "আসিফ / ও প্রিয়া তুমি কোথায়",
     img: "https://masumbillah6778.github.io/images/thumbnail/asif.jpg",
-    src: "https://masumbillah6778bd.github.io/asif/o-priya-tomi-kothai/o-priya-tomi-kothai.mp3",
+    src: "https://masumbillah6778bd.github.io/asif/o-priya-tumi-kothay/o-priya-tumi-kothay.mp3",
   },
 ];
