6922c51c339b5b50d754a2c74bca9cbf529ae0b3
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-02-11 03:43:49 +0600

    Update asif-music-list.js

--- assets/js/music-list/asif-music-list.js
+++ assets/js/music-list/asif-music-list.js
@@ -1,8 +1,8 @@
 let allMusic = [
   {
     name: "১. ও প্রিয়া তুমি কোথায়",
-    artist: "মনির খান / আবার কেনো পিছু ডাকো অ্যালবাম",
+    artist: "আসিফ / ও প্রিয়া তুমি কোথায়",
     img: "https://masumbillah6778.github.io/images/thumbnail/asif.jpg",
-    src: "https://masumbillah6778bd.github.io/monir-khan/abar-kano-pisu-dako/okanay.mp3",
+    src: "https://masumbillah6778bd.github.io/asif/o-priya-tomi-kothai/o-priya-tomi-kothai.mp3",
   },
 ];
