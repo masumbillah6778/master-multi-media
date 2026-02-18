70db4c0bda39dc74eade2a08a36362a4490a0edc
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-02-18 06:56:26 +0600

    Update image URL for music track

--- assets/js/music-list/gojol-music-list.js
+++ assets/js/music-list/gojol-music-list.js
@@ -2,7 +2,7 @@ let allMusic = [
   {
     name: "১. ৬৪ জেলা নিয়ে গান",
     artist: "নাসির / নদী অ্যালবাম",
-    img: "https://masumbillah6778.github.io/images/thumbnail/nasir.jpg",
+    img: "https://masumbillah6778.github.io/images/thumbnail/mahe-ramadan-1.jpg",
     src: "https://masumbillah6778bd.github.io/nasir/nodi/kodal-daia-mati-khura.mp3",
   },
   {
