68105079eeaab8b7230daaea6576b01aec1ab05e
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-02-19 03:31:33 +0600

    Add files via upload

---
+++ assets/js/youtube-video-player-list.js
@@ -0,0 +1,16 @@
+let videoList = document.querySelectorAll('.video-list-container .list');
+
+videoList.forEach(iframe =>{
+   iframe.onclick = () =>{
+      videoList.forEach(remove =>{remove.classList.remove('active')});
+      iframe.classList.add('active');
+      let src = iframe.querySelector('.list-video').src;
+      let title = iframe.querySelector('.list-title').innerHTML;
+      document.querySelector('.main-video-container .main-video').src = src;
+      document.querySelector('.main-video-container .main-video').play();
+      document.querySelector('.main-video-container .main-vid-title').innerHTML = title;
+   };
+});
+
+
+
