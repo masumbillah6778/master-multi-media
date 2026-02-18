77ae870649fe9a3d91cf37afbe815f59c6252285
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-02-19 02:45:02 +0600

    Modify music list with new entry and cleanup
    Updated the first music entry and removed multiple duplicate entries.

--- assets/js/music-list/gojol-music-list.js
+++ assets/js/music-list/gojol-music-list.js
@@ -1,74 +1,8 @@
 let allMusic = [
   {
-    name: "১. ৬৪ জেলা নিয়ে গান",
-    artist: "নাসির / নদী অ্যালবাম",
+    name: "১. মাহে রমজান এলো বছর ঘুর",
+    artist: "নাসির / পবিত্র মাহে রমজান",
     img: "https://masumbillah6778.github.io/images/thumbnail/mahe-ramadan.jpg",
-    src: "https://masumbillah6778bd.github.io/nasir/nodi/kodal-daia-mati-khura.mp3",
-  },
-  {
-    name: "১. ৬৪ জেলা নিয়ে গান",
-    artist: "নাসির / নদী অ্যালবাম",
-    img: "https://masumbillah6778.github.io/images/thumbnail/nasir.jpg",
-    src: "https://masumbillah6778bd.github.io/nasir/nodi/konna-alta-ranga-pai.mp3",
-  },
-  {
-    name: "১. ৬৪ জেলা নিয়ে গান",
-    artist: "নাসির / নদী অ্যালবাম",
-    img: "https://masumbillah6778.github.io/images/thumbnail/nasir.jpg",
-    src: "https://masumbillah6778bd.github.io/nasir/nodi/nodir-dhauar-pore-dhau-ase.mp3",
-  },
-  {
-    name: "১. ৬৪ জেলা নিয়ে গান",
-    artist: "নাসির / নদী অ্যালবাম",
-    img: "https://masumbillah6778.github.io/images/thumbnail/nasir.jpg",
-    src: "https://masumbillah6778bd.github.io/nasir/nodi/amai-tomare-selam-akhono-asay.mp3",
-  },
-  {
-    name: "১. ৬৪ জেলা নিয়ে গান",
-    artist: "নাসির / নদী অ্যালবাম",
-    img: "https://masumbillah6778.github.io/images/thumbnail/nasir.jpg",
-    src: "https://masumbillah6778bd.github.io/nasir/nodi/amar-barir-upor-dia.mp3",
-  },
-  {
-    name: "১. ৬৪ জেলা নিয়ে গান",
-    artist: "নাসির / নদী অ্যালবাম",
-    img: "https://masumbillah6778.github.io/images/thumbnail/nasir.jpg",
-    src: "https://masumbillah6778bd.github.io/nasir/nodi/amar-boky-joto-dokkho asay.mp3",
-  },
-  {
-    name: "১. ৬৪ জেলা নিয়ে গান",
-    artist: "নাসির / নদী অ্যালবাম",
-    img: "https://masumbillah6778.github.io/images/thumbnail/nasir.jpg",
-    src: "https://masumbillah6778bd.github.io/nasir/nodi/amar-ja-kiso-harabar.mp3",
-  },
-  {
-    name: "১. ৬৪ জেলা নিয়ে গান",
-    artist: "নাসির / নদী অ্যালবাম",
-    img: "https://masumbillah6778.github.io/images/thumbnail/nasir.jpg",
-    src: "https://masumbillah6778bd.github.io/nasir/nodi/andhar-thaky-aiso-tomi.mp3",
-  },
-  {
-    name: "১. ৬৪ জেলা নিয়ে গান",
-    artist: "নাসির / নদী অ্যালবাম",
-    img: "https://masumbillah6778.github.io/images/thumbnail/nasir.jpg",
-    src: "https://masumbillah6778bd.github.io/nasir/nodi/bokta-amar-khakha-kore.mp3",
-  },
-  {
-    name: "১. ৬৪ জেলা নিয়ে গান",
-    artist: "নাসির / নদী অ্যালবাম",
-    img: "https://masumbillah6778.github.io/images/thumbnail/nasir.jpg",
-    src: "https://masumbillah6778bd.github.io/nasir/nodi/chatar-oi-agoune-amai.mp3",
-  },
-  {
-    name: "১. ৬৪ জেলা নিয়ে গান",
-    artist: "নাসির / নদী অ্যালবাম",
-    img: "https://masumbillah6778.github.io/images/thumbnail/nasir.jpg",
-    src: "https://masumbillah6778bd.github.io/nasir/nodi/dor-thaki-condoro-sorjo-sotto.mp3",
-  },
-  {
-    name: "১. ৬৪ জেলা নিয়ে গান",
-    artist: "নাসির / নদী অ্যালবাম",
-    img: "https://masumbillah6778.github.io/images/thumbnail/nasir.jpg",
-    src: "https://masumbillah6778bd.github.io/nasir/nodi/jokhon-tomar-kou-selona.mp3",
+    src: "https://github.com/masumbillah6778/Islamicsong/raw/main/mahe_romjan%20_alo_bosor_ghure.mp3",
   },
 ];
