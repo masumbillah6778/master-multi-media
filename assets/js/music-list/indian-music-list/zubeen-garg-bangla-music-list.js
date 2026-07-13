7daac0ddc87bd4b3d569b8263dd0c6f3fb5b3337
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-07-13 18:59:23 +0600

    Add files via upload

---
+++ assets/js/music-list/indian-music-list/zubeen-garg-bangla-music-list.js
@@ -0,0 +1,48 @@
+let allMusic = [
+  {
+    name: "১. আমি টাকার এক মেসিন",
+    artist: "আকাশ মাহমুদ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/rahat-fateh-ali-khan.jpg",
+    src: "https://masumbillah6778bp.github.io/music/pakistan/rahat-fateh-ali-khan/zaroori-tha.mp3",
+  },
+  {
+    name: "২. আমি টাকার এক মেসিন",
+    artist: "আকাশ মাহমুদ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/rahat-fateh-ali-khan.jpg",
+    src: "https://masumbillah6778bp.github.io/music/pakistan/rahat-fateh-ali-khan/zaroori-tha.mp3",
+  },
+  {
+    name: "৩. আমি টাকার এক মেসিন",
+    artist: "আকাশ মাহমুদ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/rahat-fateh-ali-khan.jpg",
+    src: "https://masumbillah6778bp.github.io/music/pakistan/rahat-fateh-ali-khan/tumhe-dillagi.mp3",
+  },
+  {
+    name: "৪. আমি টাকার এক মেসিন",
+    artist: "আকাশ মাহমুদ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/rahat-fateh-ali-khan.jpg",
+    src: "https://masumbillah6778bp.github.io/music/pakistan/rahat-fateh-ali-khan/o-re-piya.mp3",
+  },
+  {
+    name: "৩০. গুরু সুভাব দাও আমার মনে",
+    artist: "আকাশ মাহমুদ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/rahat-fateh-ali-khan.jpg.jpg",
+    src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/soniye-hiriye.mp3",
+  },
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
