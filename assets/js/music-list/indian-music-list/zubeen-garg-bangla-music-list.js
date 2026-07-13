290b6c9bdf6b0b3426a9db83db1c6fbe823ba4a1
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-07-13 20:57:26 +0600

    Update zubeen-garg-bangla-music-list.js

--- assets/js/music-list/indian-music-list/zubeen-garg-bangla-music-list.js
+++ assets/js/music-list/indian-music-list/zubeen-garg-bangla-music-list.js
@@ -8,26 +8,26 @@ let allMusic = [
   {
     name: "২. আমি টাকার এক মেসিন",
     artist: "আকাশ মাহমুদ",
-    img: "https://masumbillah6778.github.io/images/thumbnail/rahat-fateh-ali-khan.jpg",
-    src: "https://masumbillah6778bp.github.io/music/pakistan/rahat-fateh-ali-khan/zaroori-tha.mp3",
+    img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/aaina-mon-bhanga.mp3
   },
   {
     name: "৩. আমি টাকার এক মেসিন",
     artist: "আকাশ মাহমুদ",
-    img: "https://masumbillah6778.github.io/images/thumbnail/rahat-fateh-ali-khan.jpg",
-    src: "https://masumbillah6778bp.github.io/music/pakistan/rahat-fateh-ali-khan/tumhe-dillagi.mp3",
+    img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/aaina-mon-bhanga.mp3
   },
   {
     name: "৪. আমি টাকার এক মেসিন",
     artist: "আকাশ মাহমুদ",
-    img: "https://masumbillah6778.github.io/images/thumbnail/rahat-fateh-ali-khan.jpg",
-    src: "https://masumbillah6778bp.github.io/music/pakistan/rahat-fateh-ali-khan/o-re-piya.mp3",
+    img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/aaina-mon-bhanga.mp3
   },
   {
     name: "৩০. গুরু সুভাব দাও আমার মনে",
     artist: "আকাশ মাহমুদ",
-    img: "https://masumbillah6778.github.io/images/thumbnail/rahat-fateh-ali-khan.jpg.jpg",
-    src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/soniye-hiriye.mp3",
+    img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/aaina-mon-bhanga.mp3
   },
 ];
 
