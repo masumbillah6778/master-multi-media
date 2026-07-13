f1532cb21dfe062cf6dd749bc8b22970e82b9312
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-07-13 21:02:38 +0600

    Update zubeen-garg-bangla-music-list.js

--- assets/js/music-list/indian-music-list/zubeen-garg-bangla-music-list.js
+++ assets/js/music-list/indian-music-list/zubeen-garg-bangla-music-list.js
@@ -3,31 +3,31 @@ let allMusic = [
     name: "১. আমি টাকার এক মেসিন",
     artist: "আকাশ মাহমুদ",
     img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
-    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/aaina-mon-bhanga.mp3",
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/tor-pirite.mp3",
   },
   {
     name: "২. আমি টাকার এক মেসিন",
     artist: "আকাশ মাহমুদ",
     img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
-    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/aaina-mon-bhanga.mp3
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/aaina-mon-bhanga.mp3",
   },
   {
     name: "৩. আমি টাকার এক মেসিন",
     artist: "আকাশ মাহমুদ",
     img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
-    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/aaina-mon-bhanga.mp3
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/ak-mutho-swapno.mp3",
   },
   {
     name: "৪. আমি টাকার এক মেসিন",
     artist: "আকাশ মাহমুদ",
     img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
-    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/aaina-mon-bhanga.mp3
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/bin-tere-tere-bin.mp3",
   },
   {
-    name: "৩০. গুরু সুভাব দাও আমার মনে",
+    name: "৫. গুরু সুভাব দাও আমার মনে",
     artist: "আকাশ মাহমুদ",
     img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
-    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/aaina-mon-bhanga.mp3
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/chokher-jole.mp3"
   },
 ];
 
