3b4700c0f1fd4d4483195442b3bc271c85c05ea1
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-07-13 19:00:53 +0600

    Update zubeen-garg-bangla-music-list.js

--- assets/js/music-list/indian-music-list/zubeen-garg-bangla-music-list.js
+++ assets/js/music-list/indian-music-list/zubeen-garg-bangla-music-list.js
@@ -3,7 +3,7 @@ let allMusic = [
     name: "১. আমি টাকার এক মেসিন",
     artist: "আকাশ মাহমুদ",
     img: "https://masumbillah6778.github.io/images/thumbnail/rahat-fateh-ali-khan.jpg",
-    src: "https://masumbillah6778bp.github.io/music/pakistan/rahat-fateh-ali-khan/zaroori-tha.mp3",
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/aaina-mon-bhanga.mp3",
   },
   {
     name: "২. আমি টাকার এক মেসিন",
