55ff86986c149c9ee0100464c612a0d8d476af8f
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-07-13 21:15:24 +0600

    Update zubeen-garg-bangla-music-list.js

--- assets/js/music-list/indian-music-list/zubeen-garg-bangla-music-list.js
+++ assets/js/music-list/indian-music-list/zubeen-garg-bangla-music-list.js
@@ -27,6 +27,99 @@ let allMusic = [
     name: "৫. চোখের জলে ভাসিয়ে দিলাম",
     artist: "জুবিন গর্গ",
     img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/chokher-jole.mp3",
+  },
+
+  {
+    name: "৬. তোর পিরিতে পাগল",
+    artist: "জুবিন গর্গ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/tor-pirite.mp3",
+  },
+  {
+    name: "৭. মন ভাঙ্গা আয়না",
+    artist: "জুবিন গর্গ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/aaina-mon-bhanga.mp3",
+  },
+  {
+    name: "৮. এক মুঠো স্বপ্ন",
+    artist: "আকাশ মাহমুদ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/ak-mutho-swapno.mp3",
+  },
+  {
+    name: "৯. বিন তেরা তেরা বিন",
+    artist: "জুবিন গর্গ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/bin-tere-tere-bin.mp3",
+  },
+  {
+    name: "১০. চোখের জলে ভাসিয়ে দিলাম",
+    artist: "জুবিন গর্গ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/chokher-jole.mp3"
+  },
+
+  {
+    name: "১. তোর পিরিতে পাগল",
+    artist: "জুবিন গর্গ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/tor-pirite.mp3",
+  },
+  {
+    name: "২. মন ভাঙ্গা আয়না",
+    artist: "জুবিন গর্গ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/aaina-mon-bhanga.mp3",
+  },
+  {
+    name: "৩. এক মুঠো স্বপ্ন",
+    artist: "আকাশ মাহমুদ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/ak-mutho-swapno.mp3",
+  },
+  {
+    name: "৪. বিন তেরা তেরা বিন",
+    artist: "জুবিন গর্গ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/bin-tere-tere-bin.mp3",
+  },
+  {
+    name: "৫. চোখের জলে ভাসিয়ে দিলাম",
+    artist: "জুবিন গর্গ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/chokher-jole.mp3"
+  },
+
+  {
+    name: "১. তোর পিরিতে পাগল",
+    artist: "জুবিন গর্গ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/tor-pirite.mp3",
+  },
+  {
+    name: "২. মন ভাঙ্গা আয়না",
+    artist: "জুবিন গর্গ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/aaina-mon-bhanga.mp3",
+  },
+  {
+    name: "৩. এক মুঠো স্বপ্ন",
+    artist: "আকাশ মাহমুদ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/ak-mutho-swapno.mp3",
+  },
+  {
+    name: "৪. বিন তেরা তেরা বিন",
+    artist: "জুবিন গর্গ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
+    src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/bin-tere-tere-bin.mp3",
+  },
+  {
+    name: "৫. চোখের জলে ভাসিয়ে দিলাম",
+    artist: "জুবিন গর্গ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
     src: "https://masumbillah6778bd.github.io/indian-music/zubeen-garg/bangla-song/chokher-jole.mp3"
   },
 ];
